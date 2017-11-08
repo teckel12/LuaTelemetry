96a058cb9ed07c2b83c1bd6730669e6793438b85
Tim Eckel <tim@leethost.com>
2017-11-07 22:43:13 -0500

    Removed stuff because Lua is stupid and adding a single value can eat 8kb

--- iNav.lua
+++ iNav.lua
@@ -4,14 +4,101 @@
 -- Docs: https://github.com/iNavFlight/LuaTelemetry
 
 -- Values that can be changed
-SHOW_CELL = false -- false = Show total battery voltage / true = Show cell average (default = false)
-BATTLOW = 3.5     -- Battery low warning cell voltage (default = 3.5)
-BATTCRIT = 3.4    -- Battery critically low warning cell voltage (default = 3.4)
-WAVPATH = "/SCRIPTS/TELEMETRY/iNav/" -- Path to iNav telemetry WAV files
+local SHOW_CELL = false -- false = Show total battery voltage / true = Show cell average (default = false)
+local WAVPATH = "/SCRIPTS/TELEMETRY/iNav/" -- Path to iNav telemetry WAV files
 
-local vars=loadfile(WAVPATH .. "iNavVars.lua")
-vars()
-vars = nil
+local FLASH = INVERS + BLINK
+local QX7 = LCD_W < 212
+local RIGHT_POS = QX7 and 129 or 195
+local GAUGE_WIDTH = QX7 and 82 or 149
+local X_CNTR_1 = QX7 and 67 or 70
+local X_CNTR_2 = QX7 and 67 or 106
+local GPS_DIGITS = QX7 and 10000 or 1000000
+
+local armed = false
+local headFree = false
+local headingHold = false
+local altHold = false
+local homeResetPrev = false
+local gpsFixPrev = false
+local altNextPlay = 0
+local battNextPlay = 0
+local battPercentPlayed = 100
+local telemFlags = -1
+
+-- Modes: t=text / f=flags for text / w=wave file
+local modes = {
+  { t="NO TELEM",  f=FLASH, w=false },
+  { t="HORIZON",   f=0,     w="hrznmd.wav" },
+  { t="ANGLE",     f=0,     w="anglmd.wav" },
+  { t="ACRO",      f=0,     w="acromd.wav" },
+  { t=" NOT OK ",  f=FLASH, w=false },
+  { t="READY",     f=0,     w="ready.wav" },
+  { t="POS HOLD",  f=0,     w="poshld.wav" },
+  { t="3D HOLD",   f=0,     w="3dhold.wav" },
+  { t="WAYPOINT",  f=0,     w="waypt.wav" },
+  { t="PASSTHRU",  f=0,     w=false },
+  { t="   RTH   ", f=FLASH, w="rtl.wav" },
+  { t="FAILSAFE",  f=FLASH, w="fson.wav" }
+}
+
+local units = { [0]="m", "V", "A", "mA", "kts", "m/s", "f/s", "kmh", "mph", "m", "ft" }
+
+local function getTelemetryId(name)
+  local field = getFieldInfo(name)
+  return field and field.id or -1
+end
+
+local function getTelemetryUnit(name)
+  local field = getFieldInfo(name)
+  return (field and field.unit <= 10) and field.unit or 1
+end
+
+local rssi, low, crit = getRSSI()
+local general = getGeneralSettings()
+local data = {
+  rssiLow = low,
+  rssiCrit = crit,
+  txBattMin = general.battMin,
+  txBattMax = general.battMax,
+  modelName = model.getInfo().name,
+  mode_id = getTelemetryId("Tmp1"),
+  rxBatt_id = getTelemetryId("RxBt"),
+  satellites_id = getTelemetryId("Tmp2"),
+  gpsAlt_id = getTelemetryId("GAlt"),
+  gpsLatLon_id = getTelemetryId("GPS"),
+  heading_id = getTelemetryId("Hdg"),
+  altitude_id = getTelemetryId("Alt"),
+  distance_id = getTelemetryId("Dist"),
+  speed_id = getTelemetryId("GSpd"),
+  current_id = getTelemetryId("Curr"),
+  altitudeMax_id = getTelemetryId("Alt+"),
+  distanceMax_id = getTelemetryId("Dist+"),
+  speedMax_id = getTelemetryId("GSpd+"),
+  currentMax_id = getTelemetryId("Curr+"),
+  batt_id = getTelemetryId("VFAS"),
+  battMin_id = getTelemetryId("VFAS-"),
+  fuel_id = getTelemetryId("Fuel"),
+  rssi_id = getTelemetryId("RSSI"),
+  rssiMin_id = getTelemetryId("RSSI-"),
+  txBatt_id = getTelemetryId("tx-voltage"),
+  ras_id = getTelemetryId("RAS"),
+  gpsAlt_unit = getTelemetryUnit("GAlt"),
+  altitude_unit = getTelemetryUnit("Alt"),
+  distance_unit = getTelemetryUnit("Dist"),
+  speed_unit = getTelemetryUnit("GSpd"),
+  modeId = 1
+}
+
+data.showCurr = data.current_id > -1 and true or false
+data.showHead = data.heading_id > -1 and true or false
+data.showAlt = data.altitude_id > -1 and true or false
+data.distPos = data.showCurr and 17 or (data.showAlt and 21 or 13)
+data.speedPos = data.showCurr and 25 or (data.showAlt and 33 or 25)
+data.battPos1 = data.showCurr and 49 or 45
+data.battPos2 = data.showCurr and 49 or 41
+data.distRef = data.distance_unit == 10 and 20 or 6
+data.altAlert = data.altitude_unit == 10 and 400 or 123
 
 local function reset()
   data.timerStart = 0
@@ -143,7 +230,7 @@ local function flightModes()
         battPercentPlayed = data.fuel
       end
     end
-    if data.fuel <= 20 or data.cell < BATTCRIT then
+    if data.fuel <= 20 or data.cell < 3.4 then
       if getTime() > battNextPlay then
         playFile(WAVPATH .. "batcrt.wav")
         if data.fuel <= 20 and battPercentPlayed > data.fuel then
@@ -156,7 +243,7 @@ local function flightModes()
         beep = true
       end
       data.battlow = true
-    elseif data.cell < BATTLOW then
+    elseif data.cell < 3.5 then
       if not data.battlow then
         playFile(WAVPATH .. "batlow.wav")
         data.battlow = true
