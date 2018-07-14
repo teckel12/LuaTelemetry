5ddcc01e0870e626042df06836f9e48ff022a84b
Tim Eckel <tim@leethost.com>
2018-07-13 23:12:15 -0400

    altitude voice notifications (vario) are working, beeping may need some tweaking

--- src/iNav.lua
+++ src/iNav.lua
@@ -116,27 +116,27 @@ local config = {
 	{ o = 1,  t = "Battery View",   c = 1, v = 1, i = 1, l = {[0] = "Cell", "Total"} },
 	{ o = 3,  t = "Cell Low",       c = 2, v = 3.5, d = true, m = 2.7, x = 3.9, i = 0.1, a = "V", b = 2 },
 	{ o = 4,  t = "Cell Critical",  c = 2, v = 3.4, d = true, m = 2.6, x = 3.8, i = 0.1, a = "V", b = 2 },
-	{ o = 14, t = "Voice Alerts",   c = 1, v = 2, x = 2, i = 1, l = {[0] = "Off", "Critical", "All"} },
-	{ o = 15, t = "Feedback",       c = 1, v = 3, x = 3, i = 1, l = {[0] = "Off", "Haptic", "Beeper", "All"} },
+	{ o = 15, t = "Voice Alerts",   c = 1, v = 2, x = 2, i = 1, l = {[0] = "Off", "Critical", "All"} },
+	{ o = 16, t = "Feedback",       c = 1, v = 3, x = 3, i = 1, l = {[0] = "Off", "Haptic", "Beeper", "All"} },
 	{ o = 9,  t = "Max Altitude",   c = 4, v = data.altitude_unit == 10 and 400 or 120, x = 9999, i = data.altitude_unit == 10 and 10 or 1, a = units[data.altitude_unit], b = 8 },
 	{ o = 13, t = "Variometer",     c = 1, v = 0, i = 1, x = 3, l = {[0] = "Off", "Display", "Beeper", "Voice"} },
-	{ o = 16, t = "RTH Feedback",   c = 1, v = 1, i = 1, l = {[0] = "Off", "On"}, b = 15 },
-	{ o = 17, t = "HeadFree Fback", c = 1, v = 1, i = 1, l = {[0] = "Off", "On"}, b = 15 },
-	{ o = 18, t = "RSSI Feedback",  c = 1, v = 1, i = 1, l = {[0] = "Off", "On"}, b = 15 },
+	{ o = 17, t = "RTH Feedback",   c = 1, v = 1, i = 1, l = {[0] = "Off", "On"}, b = 15 },
+	{ o = 18, t = "HeadFree Fback", c = 1, v = 1, i = 1, l = {[0] = "Off", "On"}, b = 15 },
+	{ o = 19, t = "RSSI Feedback",  c = 1, v = 1, i = 1, l = {[0] = "Off", "On"}, b = 15 },
 	{ o = 2,  t = "Battery Alerts", c = 1, v = 2, x = 2, i = 1, l = {[0] = "Off", "Critical", "All"} },
 	{ o = 8,  t = "Altitude Alert", c = 1, v = 1, i = 1, l = {[0] = "Off", "On"} },
 	{ o = 10, t = "Timer",          c = 1, v = 1, x = 4, i = 1, l = {[0] = "Off", "Auto", "Timer1", "Timer2", "Timer3"} },
 	{ o = 12, t = "Rx Voltage",     c = 1, v = 1, i = 1, l = {[0] = "Off", "On"} },
-	{ o = 23, t = "GPS",            c = 1, v = 0, x = 0, i = 0, l = {[0] = data.emptyGPS} },
-	{ o = 22, t = "GPS Coords",     c = 1, v = 0, x = 2, i = 1, l = {[0] = "Decimal", "Deg/Min", "Geocode"} },
+	{ o = 24, t = "GPS",            c = 1, v = 0, x = 0, i = 0, l = {[0] = data.emptyGPS} },
+	{ o = 23, t = "GPS Coords",     c = 1, v = 0, x = 2, i = 1, l = {[0] = "Decimal", "Deg/Min", "Geocode"} },
 	{ o = 7,  t = "Fuel Critical",  c = 2, v = 20, m = 5, x = 30, i = 5, a = "%", b = 2 },
 	{ o = 6,  t = "Fuel Low",       c = 2, v = 30, m = 10, x = 50, i = 5, a = "%", b = 2 },
 	{ o = 11, t = "Tx Voltage",     c = 1, v = SMLCD and 1 or 2, x = SMLCD and 1 or 2, i = 1, l = {[0] = "Number", "Graph", "Both"} },
-	{ o = 19, t = "Speed Sensor",   c = 1, v = 0, i = 1, l = {[0] = "GPS", "Pitot"} },
-	{ o = 21, t = "GPS Warning     >", c = 2, v = 3.5, d = true, m = 1.0, x = 5.0, i = 0.5, a = " HDOP" },
-	{ o = 20, t = "GPS HDOP View",  c = 1, v = 0, i = 1, l = {[0] = "Graph", "Decimal"} },
+	{ o = 20, t = "Speed Sensor",   c = 1, v = 0, i = 1, l = {[0] = "GPS", "Pitot"} },
+	{ o = 22, t = "GPS Warning     >", c = 2, v = 3.5, d = true, m = 1.0, x = 5.0, i = 0.5, a = " HDOP" },
+	{ o = 21, t = "GPS HDOP View",  c = 1, v = 0, i = 1, l = {[0] = "Graph", "Decimal"} },
 	{ o = 5,  t = "Fuel Unit",      c = 1, v = 0, i = 1, x = 2, l = {[0] = "Percent", "mAh", "mWh"} },
-	{ o = 24, t = "Vario Steps",    c = 2, v = 10, m = 5, x = 50, i = 5, a = units[data.altitude_unit] },
+	{ o = 14, t = "Vario Steps",    c = 1, v = 4, m = 1, x = 10, i = 1, l = {1, 2, 5, 10, 15, 20, 25, 30, 50}, a = units[data.altitude_unit] },
 }
 data.configCnt = 24
 for i = 1, data.configCnt do
@@ -308,14 +308,14 @@ local function flightModes()
 			else
 				beep = true
 			end
-		elseif config[7].v == 2 then -- Vario altitude
+		elseif config[7].v == 2 then -- Vario beeper
 			local tmp = math.abs(math.max(math.min(data.accZ - 1, 1), -1))
 			if tmp > 0.05 then
 				playTone(2000 * math.min(math.max(data.accZ, 0.5), 1.5), 50, 1000 - (tmp * 900), PLAY_BACKGROUND)
 			end
-		else
-			local tmp = math.floor((data.altitude + 0.5 + config[24].v / 2) / config[24].v) * config[24].v
-			if config[7].v == 3 and tmp ~= altLastAlt and data.altitude >= config[24].v / 2 and getTime() > data.altNextPlay then
+		elseif config[7].v == 3 then -- Vario voice
+			local tmp = math.floor((data.altitude + 0.5 + config[24].l[config[24].v] / 2) / config[24].l[config[24].v]) * config[24].l[config[24].v]
+			if tmp ~= data.altLastAlt and data.altitude >= config[24].l[config[24].v] / 2 and getTime() > data.altNextPlay then
 				playNumber(tmp, data.altitude_unit)
 				data.altLastAlt = tmp
 				data.altNextPlay = getTime() + 1000
