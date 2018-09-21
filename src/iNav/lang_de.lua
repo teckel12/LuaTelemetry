32f27abfe7f7b881ae0bd2b73f5c4091c88e37e2
Peschi90 <42059226+Peschi90@users.noreply.github.com>
2018-09-21 06:53:51 +0200

    Update lang_de.lua
    shorten some words.

--- src/iNav/lang_de.lua
+++ src/iNav/lang_de.lua
@@ -5,10 +5,10 @@ local modes, config = ...
 --modes[2].t  = "HORIZON"    -- HORIZON
 --modes[3].t  = "  ANGLE"    -- ANGLE
 --modes[4].t  = "   ACRO"    -- ACRO
-modes[5].t  = "NICHT OK"   -- NOT OK
+modes[5].t  = "NICH OK"   -- NOT OK
 modes[6].t  = " BEREIT"    -- READY
 --modes[7].t  = "POS HOLD"   -- POS HOLD
-modes[8].t  = "WEGPUNKT"   -- WAYPOINT
+modes[8].t  = "WEGPUN."   -- WAYPOINT
 modes[9].t  = "MANUELL"    -- MANUAL
 --modes[10].t = "   RTH   "  -- RTH
 --modes[11].t = "! FAIL !"   -- ! FAIL !
@@ -16,22 +16,22 @@ modes[12].t = " ! GAS ! "  -- ! THROT !
 --modes[13].t = " CRUISE"    -- CRUISE
 
 -- Max 16 characters
-config[1].t  = "Batterie Übersicht" -- Battery View
+config[1].t  = "Bat. Übersicht" -- Battery View
 config[2].t  = "Zelle Niedrig"      -- Cell Low
 config[3].t  = "Zelle Kritisch"     -- Cell Critical
 config[4].t  = "Sprach Alarme"      -- Voice Alerts
 config[5].t  = "Meldungen"          -- Feedback
 config[6].t  = "Max Höhe"           -- Max Altitude
 --config[7].t  = "Variometer"         -- Variometer
-config[8].t  = "RTH Meldung "       -- RTH Feedback
+config[8].t  = "RTH Meldung"       -- RTH Feedback
 config[9].t  = "HeadFree Meld."     -- HeadFree Feedback
 config[10].t = "RSSI Meldung"       -- RSSI Feedback
-config[11].t = "Batterie Warnung"   -- Battery Alert
+config[11].t = "Bat. Warnung"   -- Battery Alert
 config[12].t = "Höhen Warnung"      -- Altitude Alert
 --config[13].t = "Timer"              -- Timer
 config[14].t = "Rx Spng."           -- Rx Voltage
 --config[15].t = "GPS"                -- GPS
-config[16].t = "GPS Koordinaten"    -- GPS Coords
+config[16].t = "GPS Koordi."    -- GPS Coords
 config[17].t = "Kapaz. Kritisch"    -- Fuel Critical
 config[18].t = "Kapaz. Niedrig"     -- Fuel Low
 config[19].t = "Tx Spng."           -- Tx Voltage
@@ -41,7 +41,7 @@ config[22].t = "GPS HDOP"           -- GPS HDOP View
 config[23].t = "Kapazität"          -- Fuel Unit
 config[24].t = "Vario Schritte"     -- Vario Steps
 config[25].t = "Ansichtsmodus"      -- View Mode
-config[26].t = "AH Cntr Meldung"    -- Alt Hold Center Feedback
+config[26].t = "AlH Cntr Meld."    -- Alt Hold Center Feedback
 
 -- Max 9 characters
 config[1].l =  {[0] = "Zelle", "Total"}                    -- "Cell", "Total"
