8aeaefa4b0ef1244166daa3385aebbabca25faa3
teckel12 <tim@leethost.com>
2018-11-11 12:24:34 -0500

    Nope! No way to get GitHub and VS Code to tab align, oh well...

--- src/iNav/lang_de.lua
+++ src/iNav/lang_de.lua
@@ -16,51 +16,51 @@ modes[12].t = " ! GAS ! "	-- ! THROT !
 --modes[13].t = " CRUISE"	-- CRUISE
 
 -- Max 16 characters
-config[1].t  = "Batterie"		-- Battery View
-config[2].t  = "Zelle Niedrig"	-- Cell Low
-config[3].t  = "Zelle Kritisch"	-- Cell Critical
-config[4].t  = "Sprach Alarme"	-- Voice Alerts
-config[5].t  = "Meldungen"		-- Feedback
-config[6].t  = "Max Hoehe"		-- Max Altitude
+config[1].t  = "Batterie"			-- Battery View
+config[2].t  = "Zelle Niedrig"		-- Cell Low
+config[3].t  = "Zelle Kritisch"		-- Cell Critical
+config[4].t  = "Sprach Alarme"		-- Voice Alerts
+config[5].t  = "Meldungen"			-- Feedback
+config[6].t  = "Max Hoehe"			-- Max Altitude
 --config[7].t  = "Variometer"		-- Variometer
 config[8].t  = "RTH Meldung"		-- RTH Feedback
-config[9].t  = "HeadFree Meld."	-- HeadFree Feedback
+config[9].t  = "HeadFree Meld."		-- HeadFree Feedback
 config[10].t = "RSSI Meldung"		-- RSSI Feedback
 config[11].t = "Bat. Warnung"		-- Battery Alert
-config[12].t = "Hoehen Warnung"	-- Altitude Alert
---config[13].t = "Timer"		-- Timer
-config[14].t = "Rx Spng."		-- Rx Voltage
---config[15].t = "GPS"			-- GPS
+config[12].t = "Hoehen Warnung"		-- Altitude Alert
+--config[13].t = "Timer"			-- Timer
+config[14].t = "Rx Spng."			-- Rx Voltage
+--config[15].t = "GPS"				-- GPS
 config[16].t = "GPS Koordi."		-- GPS Coordinates
 config[17].t = "Kapaz. Kritisch"	-- Fuel Critical
-config[18].t = "Kapaz. Niedrig"	-- Fuel Low
-config[19].t = "Tx Spng."		-- Tx Voltage
-config[20].t = "Geschw. Sensor"	-- Speed Sensor
+config[18].t = "Kapaz. Niedrig"		-- Fuel Low
+config[19].t = "Tx Spng."			-- Tx Voltage
+config[20].t = "Geschw. Sensor"		-- Speed Sensor
 config[21].t = "GPS Warnung"		-- GPS Warning
-config[22].t = "GPS HDOP"		-- GPS HDOP View
-config[23].t = "Kapazitaet"		-- Fuel Unit
-config[24].t = "Vario Schritte"	-- Vario Steps
-config[25].t = "Ansichtsmodus"	-- View Mode
-config[26].t = "AlH Cntr Meld."	-- AltHold Center FB
+config[22].t = "GPS HDOP"			-- GPS HDOP View
+config[23].t = "Kapazitaet"			-- Fuel Unit
+config[24].t = "Vario Schritte"		-- Vario Steps
+config[25].t = "Ansichtsmodus"		-- View Mode
+config[26].t = "AlH Cntr Meld."		-- AltHold Center FB
 
 -- Max 9 characters
-config[1].l =  {[0] = "Zelle", "Total"}						-- "Cell", "Total"
+config[1].l =  {[0] = "Zelle", "Total"}							-- "Cell", "Total"
 config[4].l =  {[0] = "Aus", "Kritisch", "Alle"}				-- "Off", "Critical", "All"
-config[5].l =  {[0] = "Aus", "Haptisch", "Pieper", "Alle"}			-- "Off", "Haptic", "Beeper", "All"
-config[7].l =  {[0] = "Aus", "Graph", "Stimme", "Beides"}			-- "Off", "Graph", "Voice", "Both"
-config[8].l =  {[0] = "Aus", "An"}							-- "Off", "On"
-config[9].l =  {[0] = "Aus", "An"}							-- "Off", "On"
-config[10].l = {[0] = "Aus", "An"}							-- "Off", "On"
+config[5].l =  {[0] = "Aus", "Haptisch", "Pieper", "Alle"}		-- "Off", "Haptic", "Beeper", "All"
+config[7].l =  {[0] = "Aus", "Graph", "Stimme", "Beides"}		-- "Off", "Graph", "Voice", "Both"
+config[8].l =  {[0] = "Aus", "An"}								-- "Off", "On"
+config[9].l =  {[0] = "Aus", "An"}								-- "Off", "On"
+config[10].l = {[0] = "Aus", "An"}								-- "Off", "On"
 config[11].l = {[0] = "Aus", "Kritisch", "Alle"}				-- "Off", "Critical", "All"
-config[12].l = {[0] = "Aus", "An"}							-- "Off", "On"
+config[12].l = {[0] = "Aus", "An"}								-- "Off", "On"
 config[13].l = {[0] = "Aus", "Auto", "Timer1", "Timer2", "Timer3"}	-- "Off", "Auto", "Timer1", "Timer2", "Timer3"
-config[14].l = {[0] = "Aus", "An"}							-- "Off", "On"
+config[14].l = {[0] = "Aus", "An"}								-- "Off", "On"
 config[16].l = {[0] = "Dezimal", "Grad/Min"}					-- "Decimal", "Deg/Min"
 config[19].l = {[0] = "Nummer", "Graph", "Beide"}				-- "Number", "Graph", "Both"
---config[20].l = {[0] = "GPS", "Pitot"}						-- "GPS", "Pitot"
+--config[20].l = {[0] = "GPS", "Pitot"}							-- "GPS", "Pitot"
 config[22].l = {[0] = "Graph", "Dezimal"}						-- "Graph", "Decimal"
 --config[23].l = {[0] = "Percent", "mAh", "mWh"}				-- "Percent", "mAh", "mWh"
-config[25].l = {[0] = "Klassisch", "Pilot", "Radar"}				-- "Classic", "Pilot", "Radar"
-config[26].l = {[0] = "Aus", "An"}							-- "Off", "On"
+config[25].l = {[0] = "Klassisch", "Pilot", "Radar"}			-- "Classic", "Pilot", "Radar"
+config[26].l = {[0] = "Aus", "An"}								-- "Off", "On"
 
 return 0
--- src/iNav/lang_fr.lua
+++ src/iNav/lang_fr.lua
@@ -17,28 +17,28 @@ modes[12].t = " ! GAZ ! "	-- ! THROT !
 
 -- Max 16 characters
 config[1].t  = "Vue batterie"		-- Battery View
-config[2].t  = "Cellule basse"	-- Cell Low
+config[2].t  = "Cellule basse"		-- Cell Low
 config[3].t  = "Cellule critique"	-- Cell Critical
-config[4].t  = "Alerte vocale"	-- Voice Alerts
---config[5].t  = "Feedback"		-- Feedback
+config[4].t  = "Alerte vocale"		-- Voice Alerts
+--config[5].t  = "Feedback"			-- Feedback
 config[6].t  = "Altitude Maximum"	-- Max Altitude
-config[7].t  = "Variometre"		-- Variometer
+config[7].t  = "Variometre"			-- Variometer
 config[8].t  = "Feedback RTH"		-- RTH Feedback
 config[9].t  = "Feedback HeadFree"	-- HeadFree Feedback
-config[10].t = "Feedback RSSI"	-- RSSI Feedback
+config[10].t = "Feedback RSSI"		-- RSSI Feedback
 config[11].t = "Alerte batterie"	-- Battery Alert
 config[12].t = "Alerte altitude"	-- Altitude Alert
 config[13].t = "Chronometre"		-- Timer
-config[14].t = "Voltage Rx"		-- Rx Voltage
---config[15].t = "GPS"			-- GPS
+config[14].t = "Voltage Rx"			-- Rx Voltage
+--config[15].t = "GPS"				-- GPS
 --config[16].t = "Coordonnees GPS"	-- GPS Coordinates
 --config[17].t = "Fuel critique"	-- Fuel Critical
-config[18].t = "Fuel bas"		-- Fuel Low
-config[19].t = "Voltage Tx"		-- Tx Voltage
+config[18].t = "Fuel bas"			-- Fuel Low
+config[19].t = "Voltage Tx"			-- Tx Voltage
 config[20].t = "Capteur Vitesse"	-- Speed Sensor
-config[21].t = "Alerte GPS"		-- GPS Warning
+config[21].t = "Alerte GPS"			-- GPS Warning
 config[22].t = "Vue GPS HDOP"		-- GPS HDOP View
-config[23].t = "Unite Fuel"		-- Fuel Unit
+config[23].t = "Unite Fuel"			-- Fuel Unit
 config[24].t = "Avance vario"		-- Vario Steps
 config[25].t = "Mode de vue"		-- View Mode
 config[26].t = "Feedback AltHold"	-- AltHold Center FB
@@ -46,21 +46,21 @@ config[26].t = "Feedback AltHold"	-- AltHold Center FB
 -- Max 9 characters
 config[1].l =  {[0] = "Cellule", "Total"}						-- "Cell", "Total"
 config[4].l =  {[0] = "Eteint", "Critique", "Tous"}				-- "Off", "Critical", "All"
-config[5].l =  {[0] = "Eteint", "Haptique", "Beeper", "Tous"}		-- "Off", "Haptic", "Beeper", "All"
-config[7].l =  {[0] = "Eteint", "Graphique", "Voix", "Les deux"}		-- "Off", "Graph", "Voice", "Both"
+config[5].l =  {[0] = "Eteint", "Haptique", "Beeper", "Tous"}	-- "Off", "Haptic", "Beeper", "All"
+config[7].l =  {[0] = "Eteint", "Graphique", "Voix", "Les deux"}-- "Off", "Graph", "Voice", "Both"
 config[8].l =  {[0] = "Eteint", "Actif"}						-- "Off", "On"
 config[9].l =  {[0] = "Eteint", "Actif"}						-- "Off", "On"
 config[10].l = {[0] = "Eteint", "Actif"}						-- "Off", "On"
 config[11].l = {[0] = "Eteint", "Critique", "Tous"}				-- "Off", "Critical", "All"
 config[12].l = {[0] = "Eteint", "Actif"}						-- "Off", "On"
-config[13].l = {[0] = "Eteint", "Auto", "Chrono1", "Chrono2", "Chrono3"}-- "Off", "Auto", "Timer1", "Timer2", "Timer3"
+config[13].l = {[0] = "Eteint", "Auto", "Chrono1", "Chrono2", "Chrono3"}	-- "Off", "Auto", "Timer1", "Timer2", "Timer3"
 config[14].l = {[0] = "Eteint", "Actif"}						-- "Off", "On"
 --config[16].l = {[0] = "Decimal", "Deg/Min"}					-- "Decimal", "Deg/Min"
-config[19].l = {[0] = "Numerique", "Graphique", "Les deux"}			-- "Number", "Graph", "Both"
---config[20].l = {[0] = "GPS", "Pitot"}						-- "GPS", "Pitot"
+config[19].l = {[0] = "Numerique", "Graphique", "Les deux"}		-- "Number", "Graph", "Both"
+--config[20].l = {[0] = "GPS", "Pitot"}							-- "GPS", "Pitot"
 config[22].l = {[0] = "Graphique", "Decimal"}					-- "Graph", "Decimal"
 config[23].l = {[0] = "Pourcent", "mAh", "mWh"}					-- "Percent", "mAh", "mWh"
-config[25].l = {[0] = "Classique", "Pilote", "Radar"}				-- "Classic", "Pilot", "Radar"
+config[25].l = {[0] = "Classique", "Pilote", "Radar"}			-- "Classic", "Pilot", "Radar"
 config[26].l = {[0] = "Eteint", "Actif"}						-- "Off", "On"
 
 return 0
