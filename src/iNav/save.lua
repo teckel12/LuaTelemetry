a0bd3db34aca38b9887b33bd60051fbc7c6e950c
Tim Eckel <tim@leethost.com>
2020-11-19 11:23:17 -0500

    Prevent negative config values from being saved
    Fixes #389

--- src/iNav/save.lua
+++ src/iNav/save.lua
@@ -8,12 +8,12 @@ if fh == nil then
 else
 ]]
 if fh ~= nil then
-	local floor = math.floor
+	local floor, max = math.floor, math.max
 	for i = 1, #config do
 		if config[i].d == nil then
-			io.write(fh, frmt("%0" .. config[i].c .. "d", config[i].v))
+			io.write(fh, frmt("%0" .. config[i].c .. "d", max(config[i].v, 0)))
 		else 
-			io.write(fh, floor(config[i].v * 10))
+			io.write(fh, floor(max(config[i].v, 0) * 10))
 		end
 	end
 	io.close(fh)
